21c51171ad350f255c4b06cb0a7f7bb5903309db
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-25 21:00:14 +0530

    ADDED: Operators Example

--- Day1/Main.js
+++ Day1/Main.js
@@ -1,4 +1,8 @@
 var a=5 
+var b=6
 var array=[1,2,3,4,5]
 console.log(array)
 console.log(a)
+console.log(`Arithmetic Operators: +,-,*,/,%,** a=${a} b=${b} `,a+b,a-b,a*b,a/b,a%b,a**b)
+console.log("Relational Operators: <,>,<=,>=,==,!= ",a<b,a>b,a<=b,a>=b,a==b,a!=b)
+console.log("logical Operator: &&,|| ",a<b&&a<99,a<b||a<99)
